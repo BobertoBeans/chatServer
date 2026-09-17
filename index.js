ac738a47e8e8580f3b1e53bb913f4cfcf57d72e7
BobertoBeans <154119946+BobertoBeans@users.noreply.github.com>
2026-09-16 23:58:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const websock = require('ws');
+const { json } = require('express/lib/response');
 
 
 
@@ -16,35 +17,131 @@ const server = http.createServer((req,res) => {
                 res.end(data)
             }
         });
+    } else if (req.method === 'GET' && req.url === "/login") {
+        fs.readFile("login.html",(err,data) => {
+            if(err) {
+                res.writeHead(500,{"content-type" : "text/plain"});
+                res.end("server error");
+                return;
+            }else {
+                res.writeHead(200, {"content-type" : "text/html"});
+                res.end(data)
+            }
+        });
+    } else if (req.method === 'GET' && req.url === "/create") {
+        fs.readFile("create.html",(err,data) => {
+            if(err) {
+                res.writeHead(500,{"content-type" : "text/plain"});
+                res.end("server error");
+                return;
+            }else {
+                res.writeHead(200, {"content-type" : "text/html"});
+                res.end(data)
+            }
+        });
+    } else if (req.method === 'GET' && req.url === "/style.css") {
+        fs.readFile("style.css",(err,data) => {
+            if(err) {
+                res.writeHead(500,{"content-type" : "text/plain"});
+                res.end("server error");
+                return;
+            }else {
+                res.writeHead(200, {"content-type" : "text/css"});
+                res.end(data)
+            }
+        });
     }
 });
-
 const wss = new websock.Server({ server })
 
 
 
 wss.on('connection', (ws) => {
     let first = true
-    let name;
     ws.send("Enter name: ");
     ws.on('message', (message) => {
-        if (message.toString() === '?quit') {
-            ws.send("??quit??")
-            ws.close()
-        }else if (first === true) {
-            name = message.toString();
-            first = false;
-            broadcast(`${message} has entered the chat!`)
-        } else {
-            broadcast(message,name)
+        try {
+            JSON.parse(message)
+            fs.readFile("accounts.json",(err,data) => {
+                if(!err) {
+                    jason = JSON.parse(data)
+                    let str = JSON.parse(message.toString())
+                    let user = jason.find((user) => str.user === user.user)
+                    if (!user) {
+                        ws.send("E:Incorrect username")
+                    }else {
+                        if (user.pass === str.pass) {
+                            ws.send("S")
+                        }
+                    }
+                }})
+        } catch {
+            if (message.toString()[0] === '~' && message.toString()[1] === '*') {
+                let body = ''
+                for (i=2;i<message.toString().length;i++) {
+                    body += message.toString()[i]
+                }
+                fs.readFile("accounts.json",(err,data) => {
+                    if(!err) {
+                        let jason = JSON.parse(data)
+                        let str = JSON.parse(body)
+                        let user = jason.find((user) => str.user === user.user)
+                        if (!user) {
+                            jason.push(str)
+                            fs.writeFile("accounts.json",JSON.stringify(jason),err => {
+                                if (err) throw err
+                            })
+                            ws.send('S')
+                        }else {
+                            ws.send('E:Account name already exists!')
+                        }
+                    }})
+                }
+            }
+            if (message.toString() === '?quit') {
+                ws.send("??quit??")
+                ws.close()
+            }else if (first === true) {
+                let found = false;
+                ws.name = message.toString();
+                first = false;
+                for (i of wss.clients) {
+                    if (i != ws) {
+                        if (i.name === ws.name && ws._socket.remoteAddress === i._socket.remoteAddress) {
+                            found = true
+                        } else if (i.name === ws.name) {
+                            ws.send("Invalid name!")
+                        }
+                    }
+                }
+                if (!found) {
+                    try {
+                        JSON.parse(message.toString())
+                    } catch {
+                        broadcast(`${message.toString()} has entered the chat!`)
+                    }
+                }
+            } else {
+                try {
+                    JSON.parse(message.toString())
+                } catch {
+                    broadcast(message,ws.name)
+                }
+            }
+        }
+    )
+
+    ws.on('close',() => {
+        if (ws.name != undefined) {
+            try {
+                JSON.parse(ws.name)
+            } catch {
+                broadcast(`${ws.name} has left the chat`)
+            }
         }
     })
 
-    ws.on('close',() => {
-        broadcast(`${name} has left the chat`)
-    })
 })
-
 function broadcast(msg,name = null) {
     if (name) {
         for (const client of wss.clients) {
@@ -56,5 +153,6 @@ function broadcast(msg,name = null) {
         } 
     }
 }
+
 
 server.listen(8080,'0.0.0.0');
